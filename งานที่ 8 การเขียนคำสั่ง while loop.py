8516f897f1e0b5f79d73275f602441235f1a131a
Phutthikiat <139681100+mildPK@users.noreply.github.com>
2024-02-14 22:03:47 +0700

    งานที่ 8 การเขียนคำสั่ง while loop
    ทำตามโจทย์ที่ให้ไว้ในคลาสรูม

---
+++ งานที่ 8 การเขียนคำสั่ง while loop.py
@@ -0,0 +1,40 @@
+a = 1
+b = 2
+c = 100
+d = -30
+e = 15
+
+while a != 21:
+    print(a, end=",")
+    a += 2
+    if a == 21:
+        print(a)
+        break
+
+while b != 17:
+    print(b, end=",")
+    b += 3
+    if b == 17:
+        print(b)
+        break
+
+while c != -100:
+    print(c, end=",")
+    c -= 10
+    if c == -100:
+        print(c)
+        break
+
+while d != 30:
+    print(d, end=",")
+    d += 10
+    if d == 30:
+        print(d)
+        break
+   
+while e != 55:
+    print(e, end=",")
+    e += 8
+    if e == 55:
+        print(e)
+        break
